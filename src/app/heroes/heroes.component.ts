37f66cfb8079ad9cd8b7eced092f8fe7044fda8c
azarrouk <azarrouk@dxc.com>
2020-01-10 16:19:58 +0100

    change place for mockHereos

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from './mock-heroes';
-
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
