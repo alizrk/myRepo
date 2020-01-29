aadaa09a0c1d1b3a84ff16b2b5624999263218dd
azarrouk <azarrouk@dxc.com>
2020-01-29 15:28:07 +0100

    add service and messagerie service

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -15,17 +17,22 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm'
   };
 
-  heroes = HEROES;
   selectedHero: Hero;
+  heroes: Hero[];
 
 
-  constructor() { }
-
+  constructor(private heroService: HeroService) { }
+  
   ngOnInit() {
+    this.getHeroes();
   }
-
 onSelect(hero: Hero): void {
   this.selectedHero = hero;
+}
+
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
 }
 
 }
